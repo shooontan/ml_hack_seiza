579e11357e202734a1ff1f508b32969b6c70e584
shooontan <logo.bee.doo19@gmail.com>
2018-02-19 14:36:12 +0900

    webpack config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,8 +6,4 @@ module.exports = merge(webpackConfig, {
   entry: {
     bundle: [path.join(__dirname, '/src/index.js')],
   },
-  devServer: {
-    hot: true,
-    contentBase: path.join(__dirname, 'public'),
-  },
 });
